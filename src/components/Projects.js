32d07a95d5ca05a62eda4f5f98ab0759868a3df4
Eric <ericbyington33@gmail.com>
2022-11-22 19:39:11 -0500

    images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,17 +5,17 @@ export default function Projects() {
     <>
      <div className="window" id="date">
             <a href="https://ebyington.github.io/supergroup1/">
-              <img src="/public/images/DND.png" width="60%" height />
+              <img src={require("../images/DND.png")} width="60%" height />
             </a>
           </div>
           <div className="window" id="image2">
             <a href="https://viennamovies.com/">
-              <img src="/public/images/viennapictures.png" width="30%" height />
+              <img src={require("../images/viennapictures.png")} width="30%" height />
             </a>
           </div>
           <div className="window" id="image3">
             <a href="https://ebyington.github.io/Prework-study-guide/">
-              <img src="/public/images/prework.png" width="30%" height />
+              <img src={require("../images/prework.png")} width="30%" height />
             </a>
           </div>
     </>
