24bcf3332f97f6306f41b2cf2286cca9014b0e9d
Eric Byington <112353490+Ebyington@users.noreply.github.com>
2022-11-23 23:01:09 -0500

    fly

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,34 @@ import React from "react";
 export default function Projects() {
   return (
     <>
-    <div className="sectiontitle">
+    <top className="sectiontitle">
     <p>Works</p>
-    </div>
+    </top>
       <div className="row" />
+      <div className="window" id="image2">
+        <div className="col-md-4">
+          <div className="thumbnail">
+            <a href="https://viennamovies.com/">
+              <img src={require("../images/viennapictures.png")} width="70%" />
+            </a>
+            <div class="caption">
+          <p>The homepage for Vienna Pictures</p>
+        </div>
+          </div>
+        </div>
+      </div>
+      <div className="window" id="image2">
+        <div className="col-md-4">
+          <div className="thumbnail">
+            <a href="https://limitless-island-09541.herokuapp.com/">
+              <img src={require("../images/flyguyreadmdsc1.png")} width="70%" />
+            </a>
+            <div class="caption">
+          <p>Flyguys, a high-flying drone blog</p>
+        </div>
+          </div>
+        </div>
+      </div>
       <div className="window" id="date">
         <div className="col-md-4">
           <div className="thumbnail">
@@ -19,25 +43,13 @@ export default function Projects() {
           </div>
         </div>
       </div>
-      <div className="window" id="image2">
-        <div className="col-md-4">
-          <div className="thumbnail">
-            <a href="https://viennamovies.com/">
-              <img src={require("../images/viennapictures.png")} width="70%" />
-            </a>
-            <div className="caption">
-          <p>The homepage for Vienna Pictures</p>
-        </div>
-          </div>
-        </div>
-      </div>
       <div className="window" id="image3">
         <div className="col-md-4">
           <div className="thumbnail">
             <a href="https://ebyington.github.io/Prework-study-guide/">
               <img src={require("../images/prework.png")} width="70%" />
             </a>
-            <div className="caption">
+            <div class="caption">
           <p>My first project</p>
         </div>
           </div>
